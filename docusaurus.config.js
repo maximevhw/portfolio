8ed0b01f1d7cabf2310c2d3583da0eba2ab28501
maxime <maxime_vhw@hotmail.com>
2025-04-21 00:00:50 +0200

    Changed title again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Maxime_vhw',
+  title: 'Maxime Vanhoorneweder',
   tagline: 'Ethical Hacker',
   favicon: 'img/favicon.ico',
 
