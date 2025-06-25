50236dde0e6fcd1b8054d90754249fa95aa88560
maxime <maxime_vhw@hotmail.com>
2025-06-25 13:34:11 +0200

    updated homepage banner and added profilecard

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,37 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import ProfileCard from '@site/src/components/ProfileCard';
+import LetterGlitch from '@site/src/components/LetterGlitch';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+      {/* LetterGlitch as background */}
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 1
+      }}>
+        <LetterGlitch />
+      </div>        
+      <div className="container" style={{ position: 'relative', zIndex: 2 }}>
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem 0' }}>
+          <ProfileCard
+            showUserInfo={true}
+            enableTilt={true}
+            onContactClick={
+              () => window.open('https://www.linkedin.com/in/maxime-vanhoorneweder/', '_blank')
+            }
+          />
         </div>
       </div>
     </header>
@@ -33,7 +45,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Portfolio of Maxime Vanhoorneweder.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
